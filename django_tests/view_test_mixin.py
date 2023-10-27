5d76ec57b27b6feb18802d47101819984f165bac
Ian Waldron <ian.waldron@scribawaldron.com>
2023-10-27 09:56:11 -0700

    Add Django Tests

---
+++ django_tests/view_test_mixin.py
@@ -0,0 +1,24 @@
+# noinspection PyUnresolvedReferences, PyPep8Naming
+class ViewTestMixin:  # pragma: no cover
+    """
+    For use w/o user or permissions; public pages etc.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.template_name = None
+        cls.reversed_url = None
+        cls.static_url = None
+
+    def test_uses_correct_template(self):
+        # check w/o user or perms
+        self.assertIsNotNone(getattr(self, "template_name", None))
+        self.assertIsNotNone(getattr(self, "reversed_url", None))
+        response = self.client.get(self.reversed_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, self.template_name)
+
+    def test_view_url(self):
+        # response checked in correct_template so no need to use client here
+        self.assertIsNotNone(getattr(self, "static_url", None))
+        self.assertEqual(self.reversed_url, self.static_url)
