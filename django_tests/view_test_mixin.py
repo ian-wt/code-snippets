1510cd66818c6d8b95c49c18b578636c05ef52a9
Ian Waldron <ian.waldron@scribawaldron.com>
2023-11-08 11:19:44 -0800

    Update mixin for attr checks

--- django_tests/view_test_mixin.py
+++ django_tests/view_test_mixin.py
@@ -1,27 +1,27 @@
 # article for more information:
 # https://ianwaldron.com/article/20/template-tests-for-django-templateview/
 
-# noinspection PyUnresolvedReferences, PyPep8Naming
+# noinspection PyUnresolvedReferences
 class ViewTestMixin:  # pragma: no cover
     """
     For use w/o user or permissions; public pages etc.
     """
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.template_name = None
-        cls.reversed_url = None
-        cls.static_url = None
+    def _check_attr(self, attr_name):
+        self.assertIsNotNone(
+            getattr(self, attr_name, None),
+            msg=f"The required attribute '{attr_name}' has not been set causing the test to fail."
+        )
 
     def test_uses_correct_template(self):
         # check w/o user or perms
-        self.assertIsNotNone(getattr(self, "template_name", None))
-        self.assertIsNotNone(getattr(self, "reversed_url", None))
+        self._check_attr("reversed_url")
+        self._check_attr("template_name")
         response = self.client.get(self.reversed_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, self.template_name)
 
     def test_view_url(self):
+        self._check_attr("static_url")
         # response checked in correct_template so no need to use client here
-        self.assertIsNotNone(getattr(self, "static_url", None))
         self.assertEqual(self.reversed_url, self.static_url)
